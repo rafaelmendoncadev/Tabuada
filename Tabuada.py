a6685febfce9ff43797679a38e10f82ef6c16116
rafaelmendoncadev <recargaoriginal@hotmail.com>
2025-05-09 12:10:33 -0300

    Exemplo de Tabuada em Python

--- Tabuada.py
+++ Tabuada.py
@@ -1,1 +1,9 @@
-
+     # -*- coding: utf-8 -*-
+     
+numero = int(input("Digite um número: "))   
+print("Tabuada do", numero)
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")  
+   
+print("Fim da tabuada.")
